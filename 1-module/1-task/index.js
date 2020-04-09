10550534cf3eca534bf1d0aff47fd30b42a4a0f9
list109 <git_109@list.ru>
2020-04-09 13:40:36 +0300

    the solution of the second task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -3,6 +3,18 @@
  * @param {number} n
  * @returns {number}
  */
+
 function factorial(n) {
-  // ваш код...
+  if(n < 0 || n == undefined) {
+  	alert('You\'ve typed an incorrect value as the argument.');
+  	return;
+  };
+
+  let fact = 1;
+
+  while(n > 1) {
+	fact *= n--;
+  };
+
+  return fact;
 }
