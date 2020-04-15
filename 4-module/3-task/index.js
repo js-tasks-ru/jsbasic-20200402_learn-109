97331dc1d8cc8d5b563a2f52cb220be85f6167d4
list109 <git_109@list.ru>
2020-04-15 21:38:41 +0300

    the solution of all the tasks of the fourth part

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,16 @@
  * @param {Element} table
  */
 function highlight(table) {
+    Array.from(table.tBodies[0].rows).forEach(row => {
+        
+        let status = row.cells[3];
+            row.classList.add(status.dataset.available === "true" ? 'available' : 'unavailable');
+            row.hidden = !status.hasAttribute('data-available');
+
+        let gender = row.cells[2];
+            row.classList.add(gender.textContent === 'm' ? 'male' : 'female');
+
+        let age = row.cells[1];
+            row.style.textDecoration = (+age.textContent < 18) && 'line-through';
+    })
 }
