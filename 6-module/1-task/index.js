31db2a85276fdd2bf4d617676b8dbe21d8adf495
list109 <git_109@list.ru>
2020-04-28 15:26:58 +0300

    the solution of the sixth part

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -24,11 +24,42 @@
  *          age: 25,
  *          salary: '1000',
  *          city: 'Petrozavodsk'
- *      },ы
+ *      },
  *
  * @constructor
  */
+
+
 export default class UserTable {
   constructor(rows) {
+    this.elem = document.createElement('table');
+    this.elem.innerHTML = `<thead>
+                            <tr>
+                              <th>Имя</th>
+                              <th>Возраст</th>
+                              <th>Зарплата</th>
+                              <th>Город</th>
+                              <td></td>
+                            </tr>
+                            </thead>
+                            <tbody>
+                              ${createStringRows()};
+                            </tbody>`;
+
+    this.elem.onclick = removeClickedRow;
+
+    function removeClickedRow(event) {
+      if (event.target.tagName === "BUTTON") event.target.closest('tr').remove();
+    }
+
+    function createStringRows() {
+      return rows.reduce((prev, item) => {
+        prev += '<tr>';
+        for (let prop in item) {
+          prev += `<td>${item[prop]}</td>`;
+        }
+        return `${prev}<td><button>X</button></td></tr>`;
+      }, '');
+    }
   }
 }
